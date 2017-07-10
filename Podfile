df389813093e238b52711f135fedee4f535f3811
Gopal Bhojwani <gopal@Gopals-MBP.net.sahm.ae>
2017-07-10 15:51:19 +0400

    Completed email Auth

--- Podfile
+++ Podfile
@@ -1,17 +1,20 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '10.3'
 
 target 'social-media-1' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for social-media-1
-
-  pod ‘Firebase/Core’
-  pod ‘Firebase/Auth’
-  pod ‘Firebase/Database’
-  pod ‘Firebase/Storage’
-
+  
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+  pod 'FBSDKCoreKit'
+  pod 'FBSDKLoginKit'
+  pod 'FBSDKShareKit'
+  
   target 'social-media-1Tests' do
     inherit! :search_paths
     # Pods for testing
--- social-media-1/SignInVC.swift
+++ social-media-1/SignInVC.swift
@@ -7,8 +7,17 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FirebaseAuth
 
 class SignInVC: UIViewController {
+    
+    @IBOutlet weak var emailField: FancyField!
+    
+    @IBOutlet weak var pwdField: FancyField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +29,52 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil{
+                print("HIMI: Unable to authenticate with Facebook - \(String(describing: error))")
+            } else if result?.isCancelled == true{
+                print("HIMI: User cancelled Facebook Authentication")
+            } else {
+                print("HIMI: Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuthenticate(credential)
+            }
+        }
+        
+    }
+    
+    func firebaseAuthenticate(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("HIMI: Unable to authenticate with Firebase - \(String(describing: error))")
+            } else {
+                print("HIMI: Successfully authenticated with Firebase")
+            }
+        })
 
 }
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = pwdField.text {
+            Auth.auth().signIn(withEmail: email, password: pwd, completion:{ (user, error) in
+                if error == nil {
+                    print("HIMI: User Authenticated with Firebase")
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil{
+                            print("HIMI: Unable to authenticate with Firebase")
+                        } else {
+                            print ("HIMI Successfully authenticated with Firebase")
+                        }
+                })
+        }
+        
+    })
 
+}
+}
+}
